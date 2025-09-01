11764932b2d0ca17d814d1d828ef1f095fb1f9ff
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-09-01 15:49:22 +0530

    Proj 2 updated

--- project-02-class-report-generator/index.js
+++ project-02-class-report-generator/index.js
@@ -1,20 +1,57 @@
 const fs = require("fs");
+const { get } = require("http");
+const { totalmem } = require("os");
 
 const data = fs.readFileSync("./classData.json", "utf8");
 const studentData = JSON.parse(data);
 
 // console.log(studentData);
-let Total = 0;
-let Percentage = 0;
-let Grade = null;
+let total = 0;
+let percentage = 0;
+let grade = null;
+let name = null;
+let info = "";
+let totalMarks = 0;
+let avegragePercentage = 0;
 
 studentData.forEach(function (stu_info, i) {
-  console.log(stu_info.marks);
+  name = stu_info.name;
+  id = stu_info.id;
+  total = Object.values(stu_info.marks).reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  totalMarks += total;
+  percentage = (total * 100) / 300;
+  studentData.percentage = per;
+  if (percentage > 75) {
+    grade = "A";
+  } else if (percentage > 55 && percentage <= 75) {
+    grade = "B";
+  } else if (percentage > 30 && percentage <= 55) {
+    grade = "C";
+  } else if (percentage <= 30) {
+    grade = "D";
+  }
+
+  avegragePercentage = (totalMarks * 100) / 1500;
+
+  info += `${i + 1}. ${name} - total:${total} | percentage: ${Math.floor(
+    percentage
+  )}% | grade:${grade}\n`;
 });
 
-/*Individual Performance:
-1. Aarav Sharma - Total: 261 | Percentage: 87% | Grade: A
-2. Priya Das - Total: 239 | Percentage: 79% | Grade: B
-3. Rohan Mehta - Total: 206 | Percentage: 68% | Grade: C
-4. Sneha Patel - Total: 275 | Percentage: 91% | Grade: A
-5. Kabir Khan - Total: 173 | Percentage: 58% | Grade: D */
+console.log(
+  `
+--------------------------------------------
+           CLASS REPORT - AUG 2025
+--------------------------------------------
+Individual Performance:\n`,
+  info
+);
+
+/*Class Summary:
+- Class Average percentage: 76.6%
+- Top Performer: Sneha Patel (91%)
+- Students Below 70%: Rohan Mehta, Kabir Khan
+- Students with Low Attendance (<75%): Kabir Khan */
