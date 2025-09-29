f25ec5cbba13506dce9b17ffff2b31d6970920f0
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-09-29 14:59:53 +0530

    code updated

--- project-06-e-commerce-order-processor/index.js
+++ project-06-e-commerce-order-processor/index.js
@@ -4,13 +4,23 @@ const data = fs.readFileSync("data.json", "utf-8");
 const fData = JSON.parse(data);
 
 function appendText(text) {
-  fs.writeFileSync("output.txt", text, "utf-8");
+  fs.appendFileSync("output.txt", text, "utf-8");
 }
 function getrevenueByObj(obj, key, value) {
   if (obj[key] == undefined) {
     obj[key] = 0;
   }
   obj[key] += value;
+}
+
+function appendObj(object, formatStr) {
+  for (let key in object) {
+    const value = object[key];
+    let str = formatStr;
+    str = str.replaceAll("#key", key);
+    str = str.replaceAll("#value", value.toLocaleString());
+    appendText(str);
+  }
 }
 
 const revenueByCustomer = {};
@@ -35,7 +45,14 @@ appendText(`
 -----------------------------------------
          E-COMMERCE ORDER REPORT
 -----------------------------------------
-Total Revenue: ₹${totalRevenue}
+Total Revenue: ₹${totalRevenue.toLocaleString()}
 Total Orders: ${fData.length}
 Unique Customers: ${Object.keys(revenueByCustomer).length}
 `);
+
+appendText(`\nRevenue by Customer:\n`);
+appendObj(revenueByCustomer, `- #key: ₹#value\n`);
+appendText(`\nRevenue by Category:\n`);
+appendObj(revenueByCategory, `- #key: ₹#value\n`);
+appendText(`\nOrders by Status:\n`);
+appendObj(countOrderByStatus, `- #key: #value\n`);
